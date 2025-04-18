a563bba3f1c30b17360334291f4a20450b81744b
Sirionss <siriosiolos@gmail.com>
2025-04-18 20:33:09 +0200

    studing conditions and loops

--- Lecture3.selfstudy/ex1.java
+++ Lecture3.selfstudy/ex1.java
@@ -4,7 +4,7 @@ public class ex1 {
         byte b = 3;
         int c = a + b; // In java arithmetic operation result will be int if a and b were byte
         System.out.println("The sum = " + c);
-        byte y = (byte)(a+b); 
+        byte y = (byte)(a+b); // if we want the result of operation to be byte
         System.out.println("The sum = " + y);
     }
 }
---
+++ Lecture3.selfstudy/ex2.java
@@ -0,0 +1,10 @@
+public class ex2 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 3;
+        a++;
+        byte c = (byte)(a+b);
+        System.out.println("The sum = " + c);
+
+    }
+}
